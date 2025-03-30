01a97026d391fe202ffb1578f74b25ef6fa25140
Charlotte Som <charlotte@som.codes>
2025-03-30 04:03:50 +0100

    split db schema into .sql file (use rusqlite::Batch)

--- src/db.rs
+++ src/db.rs
@@ -6,46 +6,10 @@ pub type DbConnection = rusqlite::Connection;
 
 pub fn setup_db(db: &DbConnection) -> Result<()> {
     db.pragma_update(None, "journal_mode", "WAL")?;
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS counts (
-    key TEXT NOT NULL PRIMARY KEY UNIQUE,
-    count INTEGER NOT NULL
-) STRICT",
-        (),
-    )?;
-    db.execute(
-        "INSERT OR IGNORE INTO counts (key, count) VALUES ('backlinks', 0)",
-        (),
-    )?;
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS outline_rkeys (
-    id INTEGER PRIMARY KEY,
-    rkey TEXT UNIQUE NOT NULL
-) STRICT",
-        (),
-    )?;
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS outline_dids (
-    id INTEGER PRIMARY KEY,
-    did TEXT UNIQUE NOT NULL
-) STRICT",
-        (),
-    )?;
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS collections (
-    id INTEGER PRIMARY KEY,
-    collection TEXT UNIQUE NOT NULL
-) STRICT",
-        (),
-    )?;
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS data_stores (
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
-    type TEXT NOT NULL -- 'live' | 'compacted'
-) STRICT",
-        (),
-    )?;
+    let mut batch = rusqlite::Batch::new(db, include_str!("./db_schema.sql"));
+    while let Some(mut stmt) = rusqlite::fallible_iterator::FallibleIterator::next(&mut batch)? {
+        stmt.execute(())?;
+    }
 
     Ok(())
 }
---
+++ src/db_schema.sql
@@ -0,0 +1,24 @@
+-- ran every time the db is loaded
+
+CREATE TABLE IF NOT EXISTS counts (
+  key TEXT NOT NULL PRIMARY KEY UNIQUE,
+  count INTEGER NOT NULL
+) STRICT;
+INSERT OR IGNORE INTO counts (key, count) VALUES ('backlinks', 0);
+CREATE TABLE IF NOT EXISTS outline_rkeys (
+  id INTEGER PRIMARY KEY,
+  rkey TEXT UNIQUE NOT NULL
+) STRICT;
+CREATE TABLE IF NOT EXISTS outline_dids (
+  id INTEGER PRIMARY KEY,
+  did TEXT UNIQUE NOT NULL
+) STRICT;
+CREATE TABLE IF NOT EXISTS collections (
+  id INTEGER PRIMARY KEY,
+  collection TEXT UNIQUE NOT NULL
+) STRICT;
+CREATE TABLE IF NOT EXISTS data_stores (
+  id INTEGER PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  type TEXT NOT NULL -- 'live' | 'compacted'
+) STRICT;
